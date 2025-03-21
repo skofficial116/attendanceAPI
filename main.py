606934e3b4aa4b8b9a7021ef92d2a6089a5a706d
Sachin Kumar <146352431+skofficial116@users.noreply.github.com>
2025-03-21 10:48:37 +0530

    Update main.py
    Added CORS

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from fastapi import FastAPI, HTTPException
 from typing import List
 from models import AttendanceRequest, AttendanceResponse
 from calculateAttendance import calculate_attendance
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or list of allowed domains like ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def read_root():
     return {"message": "FastAPI is deployed on Render!"}
